519a262fa91843ad2e9f13aab2bbdf74466af987
travis <travislbrundage@gmail.com>
2017-12-13 18:20:47 -0800

    v0.1.1

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name = "geonode-elasticsearch-app",
-    version = "0.1",
+    version = "0.1.1",
     author = "tbrundage",
     author_email = "tbrundage@boundlessgeo.com",
     description = "Elasticsearch app for indexing GeoNode models via elasticsearch-dsl",
     long_description = "Elasticsearch app for indexing GeoNode models via elasticsearch-dsl",
     license = "GPLv2",
     url = "https://github.com/boundlessgeo/geonode-elasticsearch",
-    download_url= "https://github.com/boundlessgeo/geonode-elasticsearch/archive/0.1.tar.gz",
+    download_url= "https://github.com/boundlessgeo/geonode-elasticsearch/archive/0.1.1.tar.gz",
     packages = find_packages(),
     classifiers = [
         "Environment :: Web Environment",
