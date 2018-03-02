553083edf6ac50d821d7d1d6366f28d72fc97a34
Amiram Rahav <arahav@boundlessgeo.com>
2018-03-01 19:25:13 -0500

    Bump to 0.2.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except IOError:
 
 setup(
     name = "geonode-elasticsearch-app",
-    version = "0.2.1",
+    version = "0.2.2",
     author = "tbrundage",
     author_email = "tbrundage@boundlessgeo.com",
     description = "Elasticsearch app for indexing GeoNode models via elasticsearch-dsl",
